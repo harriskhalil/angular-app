f23b5dc66966d7ce28913c5cad15896e381bb1b2
haris <hm@dicube.at>
2025-11-12 17:57:43 +0500

    adding guards

--- src/app/guards/auth-guard.ts
+++ src/app/guards/auth-guard.ts
@@ -2,8 +2,6 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
 export const AuthGuard: CanActivateFn = (route, state) => {
-  console.log(route);
-  console.log(state);
   
   
   const router = inject(Router);
